e2b9a865a5be32e79978ede1d75c69e7e31fdb9d
rainu <rainu@raysha.de>
2014-10-26 15:47:21 +0100

    implements a AES secured Server

---
+++ src/main/java/de/raysha/net/scs/AESServer.java
@@ -0,0 +1,80 @@
+package de.raysha.net.scs;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.InvalidKeyException;
+
+import javax.crypto.SecretKey;
+
+/**
+ * This {@link AbstractServer} establish a {@link AESConnector} for each incoming connections.
+ *
+ * @author rainu
+ */
+public abstract class AESServer extends AbstractServer {
+	private final SecretKey secretKey;
+	private final String stringKey;
+
+	public AESServer(SecretKey key, int port) throws IOException, InvalidKeyException {
+		super(port);
+
+		this.secretKey = key;
+		this.stringKey = null;
+		checkKey();
+	}
+
+	public AESServer(SecretKey key, ServerSocket socket) throws InvalidKeyException {
+		super(socket);
+
+		this.secretKey = key;
+		this.stringKey = null;
+		checkKey();
+	}
+
+	public AESServer(String key, int port) throws IOException, InvalidKeyException {
+		super(port);
+
+		this.secretKey = null;
+		this.stringKey = key;
+		checkKey();
+	}
+
+	public AESServer(String key, ServerSocket socket) throws InvalidKeyException {
+		super(socket);
+
+		this.secretKey = null;
+		this.stringKey = key;
+		checkKey();
+	}
+
+	private void checkKey() throws InvalidKeyException {
+		createConnector(null);
+	}
+
+	@Override
+	protected void handleNewSocket(Socket newSocket) {
+		AESConnector connector;
+		try {
+			connector = createConnector(newSocket);
+		} catch (InvalidKeyException e) {
+			throw new IllegalStateException("The key should be never changed!", e);
+		}
+
+		handleNewConnetion(connector);
+	}
+
+	private AESConnector createConnector(Socket socket) throws InvalidKeyException {
+		AESConnector connector;
+		if(secretKey != null){
+			connector = new AESConnector(socket, secretKey);
+		}else{
+			connector = new AESConnector(socket, stringKey);
+		}
+
+		return connector;
+	}
+
+	protected abstract void handleNewConnetion(AESConnector connector);
+
+}
---
+++ src/test/java/de/raysha/net/scs/AESServerTest.java
@@ -0,0 +1,35 @@
+package de.raysha.net.scs;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.InvalidKeyException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class AESServerTest {
+
+	@Test
+	public void newConnection() throws IOException, InterruptedException, InvalidKeyException{
+		ServerSocket socket = new ServerSocket(0);
+		final List<AESConnector> list = new ArrayList<AESConnector>();
+
+		AESServer server = new AESServer("secret", socket) {
+			@Override
+			protected void handleNewConnetion(AESConnector connector) {
+				list.add(connector);
+			}
+		};
+		server.start();
+
+		//client connect
+		new Socket("localhost", socket.getLocalPort());
+		Thread.sleep(500);
+
+		assertEquals(1, list.size());
+	}
+}
